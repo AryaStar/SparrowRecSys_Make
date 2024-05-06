52c5da095d1a260ebaadac453f3e9407c18b6bb2
AryaStar <757451920@qq.com>
2024-05-07 01:30:27 +0800

    update config

--- src/main/java/com/sparrowrecsys/online/util/Config.java
+++ src/main/java/com/sparrowrecsys/online/util/Config.java
@@ -5,8 +5,8 @@ public class Config {
     public static final String DATA_SOURCE_FILE = "file";
 
     public static String EMB_DATA_SOURCE = Config.DATA_SOURCE_FILE;
-    public static boolean IS_LOAD_USER_FEATURE_FROM_REDIS = false;
-    public static boolean IS_LOAD_ITEM_FEATURE_FROM_REDIS = false;
+    public static boolean IS_LOAD_USER_FEATURE_FROM_REDIS = true;
+    public static boolean IS_LOAD_ITEM_FEATURE_FROM_REDIS = true;
 
     public static boolean IS_ENABLE_AB_TEST = false;
 
